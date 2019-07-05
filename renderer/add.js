5c63c84cc5d2598690ceb8f39aabfc83d5f87974
Daltimore <obinnaleo78@gmail.com>
2019-07-05 15:56:52 +0100

    fixed disabled btn

--- renderer/add.js
+++ renderer/add.js
@@ -1,6 +1,12 @@
 'use strict'
 
 const { ipcRenderer } = require('electron')
+const notifier = require('node-notifier')
+const $ = require('jquery')
+
+$(document).ready(function() {
+
+  $("#my-btn").attr("disabled", true);
 
 document.getElementById('todoForm').addEventListener('submit', (evt) => {
   // prevent default refresh functionality of forms
@@ -9,9 +15,38 @@ document.getElementById('todoForm').addEventListener('submit', (evt) => {
   // input on the form
   const input = evt.target[0]
 
+
   // send todo to main process
-  ipcRenderer.send('add-todo', input.value)
+  
+
+  if(input.value == '') {
+    document.getElementById('error').innerHTML = "This is required!!!"
+  } else {
+    $("#my-btn").attr("disabled", false);
+    ipcRenderer.send('add-todo', input.value)
+    document.getElementById('my-btn').onclick = () => {
+      notifier.notify ({
+         title: 'Success',
+         message: 'Todo Added'
+      
+      }, function (err, response) {
+         // Response is response from notification
+      });
+
+      notifier.on('click', function (notifierObject, options) {
+         console.log("You clicked on the notification")
+      });
+
+      notifier.on('timeout', function (notifierObject, options) {
+         console.log("Notification timed out!")
+      });
+   }
+   
+  }
+  
 
   // reset input
   input.value = ''
+})
+
 })
